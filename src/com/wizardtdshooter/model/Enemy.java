4cb3d83d47f9397a34e80873f4db630d9a176b5b
derickfelix <derickfelix4321@gmail.com>
2017-08-29 13:49:02 -0400

    Enemy - Improved Intelligence

--- src/com/wizardtdshooter/model/Enemy.java
+++ src/com/wizardtdshooter/model/Enemy.java
@@ -3,7 +3,6 @@ package com.wizardtdshooter.model;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.util.Random;
 
 import com.wizardtdshooter.controller.Handler;
 import com.wizardtdshooter.controller.SpriteSheet;
@@ -11,17 +10,17 @@ import com.wizardtdshooter.controller.SpriteSheet;
 public class Enemy extends GameObject {
 
 	private Handler handler;
-	private Random r;
 	private BufferedImage enemy_image;
-	private int choose;
+	private int persuitTimer;
 	private int hp;
+	private boolean isMoving;
 
 	public Enemy(int x, int y, ID id, Handler handler, SpriteSheet ss) {
 		super(x, y, id, ss);
-		this.choose = 0;
+		this.persuitTimer = 0;
 		this.hp = 100;
 		this.handler = handler;
-		this.r = new Random();
+		this.isMoving = false;
 		this.enemy_image = ss.grabImage(4, 1, 32, 32);
 	}
 
@@ -30,24 +29,18 @@ public class Enemy extends GameObject {
 		this.x += this.velX;
 		this.y += this.velY;
 
-		choose = r.nextInt(10);
-
+		if (!isMoving) {
+			this.persuitTimer++;
+		}
 		for (int i = 0; i < this.handler.object.size(); i++) {
 			GameObject tempObject = this.handler.object.get(i);
 
 			if (tempObject.getId() == ID.Block) {
 				if (this.getBoundsBig().intersects(tempObject.getBounds())) {
-					//this.x += (velX * 2) * -1;
-					//this.y += (velY * 2) * -1;
-					
-					 this.x += velX * -1;
-				     this.y += velY * -1;
-				     this.velX = 0;
-						this.velY = 0;
-				} else if (choose == 0) {
-					this.velX = (r.nextInt(4 - -4) + -4);
-					this.velY = (r.nextInt(4 - -4) + -4);
+					this.turnBack();
+					isMoving = false;
 				}
+
 			}
 
 			if (tempObject.getId() == ID.Bullet) {
@@ -61,12 +54,50 @@ public class Enemy extends GameObject {
 				if (this.getBounds().intersects(tempObject.getBounds())) {
 
 				}
+				if (this.isMoving) {
+					this.persuitPlayer(tempObject);
+				}
 			}
 		}
 
 		if (this.hp <= 0) {
 			this.handler.removeObject(this);
 		}
+		// end hp if
+		if (this.persuitTimer >= 20) {
+			this.isMoving = true;
+			this.persuitTimer = 0;
+		}
+	}
+
+	private void turnBack() {
+		this.velX *= -1;
+		this.velY *= -1;
+	}
+
+	private void persuitPlayer(GameObject player) {
+		if(this.x - player.x  < 400 && this.y - player.y < 400) {
+			if (this.x > player.getX()) {
+				this.velX = -1;
+
+			}
+			if (this.x < player.getX()) {
+				this.velX = 1;
+
+			}
+			if (this.y > player.getY()) {
+				this.velY = -1;
+
+			}
+			if (this.y < player.getY()) {
+				this.velY = 1;
+
+			}
+		} else {
+			this.velX = 0;
+			this.velY = 0;
+		}
+		
 	}
 
 	@Override
@@ -82,5 +113,4 @@ public class Enemy extends GameObject {
 	public Rectangle getBoundsBig() {
 		return new Rectangle(x - 16, y - 16, 64, 64);
 	}
-
 }
